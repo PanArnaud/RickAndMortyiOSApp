bf3cf763fbe4a1ce631ad8cac1cfa0d9cca6574d
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2023-02-27 00:14:22 +0400

    start to add models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Arnaud Panapadéatchy on 27/02/2023.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case genderless = "Genderless"
+    case `unknown` = "unknown"
+}
---
+++ RickAndMorty/Models/RMCharacterStatus.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  RickAndMorty
+//
+//  Created by Arnaud Panapadéatchy on 27/02/2023.
+//
+
+import Foundation
+
+enum RMCharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "unknown"
+}
